7b12c817a6e8b05be9f1cb5c3cddd74c7b7d2d6f
francisudeji <francis.udejiofficial@gmail.com>
2019-04-05 07:05:55 +0100

    Initial commit message with react-static

---
+++ static.config.js
@@ -0,0 +1,45 @@
+import path from "path";
+import axios from "axios";
+
+export default {
+  getRoutes: async () => {
+    const { data: posts } = await axios.get(
+      "https://jsonplaceholder.typicode.com/posts"
+    );
+
+    return [
+      {
+        path: "/blog",
+        template: "src/pages/blog",
+        getData: () => ({
+          posts
+        }),
+        children: posts.map(post => ({
+          path: `/post/${post.id}`,
+          template: "src/containers/Post",
+          getData: () => ({
+            post
+          })
+        }))
+      },
+      {
+        path: "/",
+        template: "src/pages/index"
+      },
+      {
+        path: "/about",
+        template: "src/pages/about"
+      }
+    ];
+  },
+  plugins: [
+    [
+      require.resolve("react-static-plugin-source-filesystem"),
+      {
+        location: path.resolve("./src/pages")
+      }
+    ],
+    require.resolve("react-static-plugin-reach-router"),
+    require.resolve("react-static-plugin-sitemap")
+  ]
+};
